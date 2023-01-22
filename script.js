f53aa9836af205fdd0c21c8facaf58984eb675f5
renn27 <119906641+renn27@users.noreply.github.com>
2023-01-22 17:55:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,21 @@
 function hasil() {
     let nama1 = document.getElementById("nama1").value;
     let nama2 = document.getElementById("nama2").value;
-    document.getElementById("hasil").innerHTML = nama1 + nama2 + Math.floor(Math.random() * 100) ;
+    
+    document.getElementById("nm1").innerHTML =nama1;
+    document.getElementById("nm2").innerHTML =nama2;
+    document.getElementById("persentase").innerHTML =Math.floor(Math.random() * 100)+"%" ;
 };
 
 $(function() {
+   
     $(".btn").click(function() {
-        hasil();
-        $(".hasil").slideToggle(500);
+        let nama1 = document.getElementById("nama1").value;
+        let nama2 = document.getElementById("nama2").value;
+        if (nama1&&nama2!="") {
+            hasil();
+            $(".hasil").show(500);
+            $(".hasil").css({'display' : 'block'});
+        }
     });
 });
